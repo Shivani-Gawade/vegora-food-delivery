84154f0b12a77a1abf7dc36860f968e29d03f75e
Shivani-Gawade <shivanigawade490@gmail.com>
2026-06-26 20:17:01 +0530

    connected to DB

--- server/config/db.js
+++ server/config/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
     const uri =
       "mongodb+srv://vegora:vegora123@cluster0.bxcwq86.mongodb.net/?appName=Cluster0";
     const conn = await mongoose.connect(uri);
-    console.log(`MongoDB connected: ${conn.connection.host}`);
+    console.log(`MongoDB connected: ${conn.connection.name}`);
   } catch (error) {
     console.log(`Connection failed ${error.message}`);
   }

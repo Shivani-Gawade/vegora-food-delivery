04969b4958139493bbc63d8bd8aff19526944f36
Shivani-Gawade <shivanigawade490@gmail.com>
2026-06-28 16:06:31 +0530

    add bcrypt and salt in user model

--- server/models/User.js
+++ server/models/User.js
@@ -29,4 +29,20 @@ const userSchema = new mongoose.Schema(
   },
 );
 
+userSchema.pre("save", async function (next) {
+  const person = this;
+  if (!person.isModified("password")) {
+    return next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hashPass = await bcrypt.hash(this.password, salt);
+
+  this.password = hashPass;
+});
+
+userSchema.methods.matchPassword = async function (enteredPass) {
+  return await bcrypt.compare(enteredPass, this.password);
+};
+
 module.exports = mongoose.model("User", userSchema);

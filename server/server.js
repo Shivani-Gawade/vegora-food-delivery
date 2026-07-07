4bd9609a38bba59f879d8d22572c625606f007c9
Shivani-Gawade <shivanigawade490@gmail.com>
2026-07-07 18:42:06 +0530

    create restaurant module

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ const express = require("express");
 require("dotenv").config();
 
 const connectDB = require("./config/db");
-const router = require("./routes/authRoute");
 
 const app = express();
 
@@ -10,8 +9,7 @@ connectDB();
 app.use(express.json());
 
 //route
-app.use("/", router);
-// app.use("/auth", require("./routes/authRoute"));
+app.use("/auth", require("./routes/authRoute"));
 
 app.listen(3000, () => {
   console.log("Server is running on 3000");
